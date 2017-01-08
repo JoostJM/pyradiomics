fa6b3cf38a8a63743e3af43df84c31b2093d2f86
Joost van Griethuysen <j.v.griethuysen@nki.nl>
2017-01-08 13:35:08 -0500

    ENH: Add matrix calculation in C for GLDM

--- radiomics/src/cmatrices.h
+++ radiomics/src/cmatrices.h
@@ -1,4 +1,5 @@
 int calculate_glcm(int *image, char *mask, int Sx, int Sy, int Sz, int *angles, int Na, double *glcm, int Ng);
+int calculate_gldm(int *image, char *mask, int Sx, int Sy, int Sz, int *angles, int Na, double *gldm, int Ng, int alpha);
 int calculate_glszm(int *image, signed char *mask, int Sx, int Sy, int Sz, int *angles, int Na, int *tempData, int Ng, int Ns);
 int fill_glszm(int *tempData, double *glszm, int Ng, int maxRegion);
 int calculate_glrlm(int *image, char *mask, int *size, int *strides, int *angles, int Na, double *glrlm, int Ng, int Nr);
